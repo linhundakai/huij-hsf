2ffb2350653609daff13c88b8f495972ceaf9be4
Administrator <Administrator@oo3the4>
2017-07-18 14:09:13 +0800

    test1

---
+++ huij-hsf-parent/huij-hsf-provider/src/main/java/org/huij/hsf/provider/service/impl/TestService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.huij.hsf.provider.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Administrator
+ *
+ */
+@Service
+public class TestService {
+
+	public void test() {
+		System.err.println("TestService");
+	}
+}
