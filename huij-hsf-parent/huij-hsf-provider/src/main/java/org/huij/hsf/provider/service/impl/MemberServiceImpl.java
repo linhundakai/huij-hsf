498470fdffdf5cc0fd89ffbcb8bc5c784c8a6b0d
Administrator <Administrator@oo3the4>
2017-07-18 14:10:13 +0800

    1

--- huij-hsf-parent/huij-hsf-provider/src/main/java/org/huij/hsf/provider/service/impl/MemberServiceImpl.java
+++ huij-hsf-parent/huij-hsf-provider/src/main/java/org/huij/hsf/provider/service/impl/MemberServiceImpl.java
@@ -3,14 +3,20 @@ package org.huij.hsf.provider.service.impl;
 
 import org.huij.hsf.api.domain.Member;
 import org.huij.hsf.api.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.boot.hsf.annotation.HSFProvider;
 
 @HSFProvider(serviceInterface = MemberService.class, serviceGroup = "HSF", clientTimeout = 3000, serviceVersion = "1.0.0")
 public class MemberServiceImpl implements MemberService {
 
+	@Autowired
+	private TestService testService;
+	
 	@Override
 	public Member getMember(Long id) {
+		testService.test();
+		
 		Member m = new Member();
 		m.setAddr("杭州");
 		m.setId(id);
